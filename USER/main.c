8dcadaa72229cd35651f7fae098b9d822b28e838
zhs123456 <646625434@qq.com>
2016-08-06 21:25:10 +0800

    加了一个变量cunnt1
    用来测试程序修改后github desktop与github之间的变化

--- USER/main.c
+++ USER/main.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	u16 count=0;
+	u16 count1=0;
 	u8 control=1;
 	KEY_Confrigration();
 	TIM1_PWM_Confriguration();//声明TIM1初始化函数
@@ -16,6 +17,7 @@ int main(void)
 		if(count>450)control=0;
 		if(count==0) control=1;
 		TIM_SetCompare1(TIM1,count);//实时改变PWM的占空比
+		count1++;
 	}
 }
 
